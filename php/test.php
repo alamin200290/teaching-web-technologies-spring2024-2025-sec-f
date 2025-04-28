2feea94661262089ffd3fe76f4b3e0dc5e6cc02e
Md.Al-Amin <amin.se.aiub@gmail.com>
2025-04-28 14:11:06 +0600

    Add files via upload

---
+++ php/test.php
@@ -0,0 +1,67 @@
+<?php
+
+$id = 10;
+$name = "alamin";
+$cgpa = 3.5;
+
+$std = array(1, 3.5, 'alamin');
+// $std = [1, 3.5, 'alamin'];
+// $std[1];
+
+// $stds = [
+//     [1, 3.5, 'alamin'],
+//     [2, 3.5, 'alamin'],
+//     [3, 3.5, 'alamin']
+// ];
+
+// $stds[2][1];
+
+// $std = ['id'=> 1, 'cgpa'=>3.5, 'name'=>'alamin'];
+// $std['name'];
+
+// $stds =[
+//     ['id'=> 1, 'cgpa'=>3.5, 'name'=>'alamin'],
+//     ['id'=> 2, 'cgpa'=>2.5, 'name'=>'abc'],
+//     ['id'=> 3, 'cgpa'=>1.5, 'name'=>'xyz']
+// ];
+
+// $stds[2]['cgpa'];
+
+// $stds =[
+//     's1'=> ['id'=> 1, 'cgpa'=>3.5, 'name'=>'alamin'],
+//     's2'=> ['id'=> 2, 'cgpa'=>2.5, 'name'=>'abc'],
+//     's3'=> ['id'=> 3, 'cgpa'=>1.5, 'name'=>'xyz']
+// ];
+
+// $stds['s2']['name'];
+
+// function abc($a=0, $b=20){
+//     return $a+$b;
+// }
+
+// $c = abc( 0 , 20);
+
+// for($i = 0; $i<10; $i++){
+
+// }
+
+// foreach( $std as $v){
+
+// }
+
+// if(){
+
+// }else{
+
+// }
+
+//echo "Hello {$name} PHP". $id;
+//print($id);
+
+// for($i = 0; $i<10; $i++){
+//     echo "<h1>Webtech </h1>";
+// }
+
+//print_r($std);
+var_dump($std);
+?>
